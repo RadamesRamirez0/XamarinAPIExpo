a8756fb6ed7c52678ef38b725c4439f5f02f79d8
Radames Ramirez <rramirez31@ucol.mx>
2021-10-21 01:04:43 -0500

    first commit

---
+++ AppApiLibre/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Newtonsoft.Json;
+
+namespace AppApiLibre
+{
+    public partial class MainPage : ContentPage
+    {
+       /* private const string Url = "https://api.openweathermap.org/data/2.5/weather?id=4013516&appid=28aa79cd4763b27e3d266a664c752607&units=metric";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<ModelClima> _clima;*/
+        public MainPage()
+        {            
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("https://dataservice.accuweather.com/currentconditions/v1/topcities/50?apikey=hgFa3lcUmKupPRcxblkW8wVATVFrqKQY&language=es-mx");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accept","application/json");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                var resultado = JsonConvert.DeserializeObject<List<ModelClima>>(content);
+                
+
+                MyListView.ItemsSource = resultado;
+            }
+
+            /*
+            string content = await client.GetStringAsync(Url);
+            List<ModelClima> climas = JsonConvert.DeserializeObject<List<ModelClima>>(content);
+            _clima = new ObservableCollection<ModelClima>(climas);
+            MyListView.ItemsSource = _clima;
+            base.OnAppearing();*/
+        }
+        
+
+    }
+}
---
+++ AppApiLibre/ModelClima.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppApiLibre
+{
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Country
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+        public string EnglishName { get; set; }
+    }
+
+    public class TimeZone
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double GmtOffset { get; set; }
+        public bool IsDaylightSaving { get; set; }
+        public DateTime? NextOffsetChange { get; set; }
+    }
+
+    public class Metric
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Imperial
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+        public int UnitType { get; set; }
+    }
+
+    public class Elevation
+    {
+        public Metric Metric { get; set; }
+        public Imperial Imperial { get; set; }
+    }
+
+    public class GeoPosition
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public Elevation Elevation { get; set; }
+    }
+
+    public class Temperature
+    {
+        public Metric Metric { get; set; }
+        public Imperial Imperial { get; set; }
+    }
+
+    public class LocalSource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string WeatherCode { get; set; }
+    }
+
+    public class ModelClima
+    {
+        public string Key { get; set; }
+        public string LocalizedName { get; set; }
+
+        public string CityCountry { get { return LocalizedName + ", " + Country.LocalizedName; } }
+
+        public string ClimaUnidad { get { return Temperature.Metric.Value.ToString() + " °" + Temperature.Metric.Unit; } }
+        public string EnglishName { get; set; }
+        public Country Country { get; set; }
+        public TimeZone TimeZone { get; set; }
+        public GeoPosition GeoPosition { get; set; }
+        public DateTime LocalObservationDateTime { get; set; }
+        public int EpochTime { get; set; }
+        public string WeatherText { get; set; }
+        public int WeatherIcon { get; set; }
+        public bool HasPrecipitation { get; set; }
+        public string PrecipitationType { get; set; }
+        public bool IsDayTime { get; set; }
+        public Temperature Temperature { get; set; }
+        public string MobileLink { get; set; }
+        public string Link { get; set; }
+        public LocalSource LocalSource { get; set; }
+    }
+
+
+
+
+}
